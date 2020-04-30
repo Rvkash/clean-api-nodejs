fa6f350ce26bb3e45f043c847a4f067e074be822
Rukash <rafael.ieel@hotmail.com>
2020-04-30 15:04:50 -0300

    ensure Login Router return 200

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -17,7 +17,10 @@ module.exports =
      if (!password) {
        return HttpResponse.badRequest('password')
      }
-     this.authUseCase.auth(email, password)
-     return HttpResponse.unauthorizedError()
+     const accessToken = this.authUseCase.auth(email, password)
+     if (!accessToken) {
+       return HttpResponse.unauthorizedError()
+     }
+     return HttpResponse.ok()
    }
  }
--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -7,9 +7,11 @@ const makeSut = () => {
     auth (email, password) {
       this.email = email
       this.password = password
+      return this.accessToken
     }
   }
   const authUseCaseSpy = new AuthUseCase()
+  authUseCaseSpy.accessToken = 'valid_token'
   const sut = new LoginRouter(authUseCaseSpy)
   return {
     sut,
@@ -68,7 +70,8 @@ describe('Login Router', () => {
   })
 
   test('Should return 401 when invalid credentials are provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
+    authUseCaseSpy.accessToken = null
     const httpRequest = {
       body: {
         email: 'invalid_email@mail.com',
@@ -80,11 +83,24 @@ describe('Login Router', () => {
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
 
+  test('Should return 200 when valid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'valid_email@email.com',
+        password: 'valid_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+  })
+
   test('Should return 500 if no AuthUseCase provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
-        email: 'any_@mail.com',
+        email: 'any_@email.com',
         password: 'any_password'
       }
     }
